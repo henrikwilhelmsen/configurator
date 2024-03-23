71acb6818cae44b75a81791ea2349df7dea9f79f
Henrik Wilhelmsen <contact@henrikwilhelmsen.com>
2024-03-23 15:28:42 +0100

    Move paths to separate module, check for existence

--- src/configurator/installer/setup.py
+++ src/configurator/installer/setup.py
@@ -1,70 +1,21 @@
 from pathlib import Path
-from subprocess import CalledProcessError, check_output
 
 from result import Err, Ok, Result
 
 from configurator.installer.config import InstallerConfig
 from configurator.installer.copy import CopyInstaller
+from configurator.installer.paths import get_flow_config_dir, get_powershell_config_dir, get_win_terminal_config_dir
 from configurator.installer.protocol import Installer
 from configurator.installer.terminal import TerminalInstaller
 from configurator.settings import get_settings
 from configurator.util import in_linux, in_windows
 
 
-def get_powershell_dir() -> Result[Path, str]:
-    """Get the PowerShell user directory located in <user_paths>/Documents/PowerShell."""
-    try:
-        documents_dir = check_output(
-            args=["powershell.exe", "[Environment]::GetFolderPath('MyDocuments')"],
-            encoding="utf-8",
-            shell=True,
-        ).splitlines()[0]
-
-    except CalledProcessError as e:
-        return Err(e.output)
-
-    powershell_dir = Path(documents_dir) / "PowerShell"
-
-    if powershell_dir.exists():
-        return Ok(powershell_dir)
-
-    return Err("Powershell directory does not exist.")
-
-
-def get_win_terminal_config_dir() -> Path:
-    """Get the path to the Windows Terminal settings.json file."""
-    return Path.home().joinpath(
-        "AppData",
-        "Local",
-        "Packages",
-        "Microsoft.WindowsTerminal_8wekyb3d8bbwe",
-        "LocalState",
-    )
-
-
-def get_flow_config() -> Result[Path, str]:
-    """Get the path to the Flow Launcher Settings.json file. (When installed with Scoop).
-
-    C:/Users/<username>/scoop/apps/flow-launcher/current/app-<version>/UserData/Settings/
-    """
-    flow_dir = Path.home() / "scoop" / "apps" / "flow-launcher" / "current"
-    config_dir: Path | None = None
-
-    for p in flow_dir.glob("*"):
-        if p.is_dir() and p.name.startswith("app-"):
-            config_dir = p / "UserData" / "Settings"
-
-    if config_dir and config_dir.exists():
-        return Ok(config_dir)
-
-    return Err("Flow config not found.")
-
-
 def powershell_installer() -> Result[Installer, str]:
     settings = get_settings()
     source = settings.data_repo_dir / "powershell"
 
-    match get_powershell_dir():
+    match get_powershell_config_dir():
         case Ok(v):
             target = v
         case Err(e):
@@ -77,9 +28,14 @@ def powershell_installer() -> Result[Installer, str]:
 def terminal_installer() -> Result[Installer, str]:
     settings = get_settings()
     source = settings.data_repo_dir / "terminal"
-    target = get_win_terminal_config_dir()
-    installer_config = InstallerConfig(name="terminal", source=source, target=target)
 
+    match get_win_terminal_config_dir():
+        case Ok(v):
+            target = v
+        case Err(e):
+            return Err(f"Could not get terminal dir: {e}")
+
+    installer_config = InstallerConfig(name="terminal", source=source, target=target)
     return Ok(TerminalInstaller(config=installer_config))
 
 
@@ -87,7 +43,7 @@ def flow_installer() -> Result[Installer, str]:
     settings = get_settings()
     source = settings.data_repo_dir / "flow"
 
-    match get_flow_config():
+    match get_flow_config_dir():
         case Ok(v):
             target = v
         case Err(e):
